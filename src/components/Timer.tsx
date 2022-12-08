0ee42376faa734714ac81c9cb2454e1b1b140f4d
PlewkaMaciej <82128422+PlewkaMaciej@users.noreply.github.com>
2022-12-08 16:30:17 +0100

    add transition

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,22 +1,54 @@
 import "../styles/timer.css";
+import React, { useRef } from "react"
 interface Props {
 
     mainPageRef: React.RefObject<HTMLDivElement | null>
 }
+
 const Timer: React.FC<Props> = ({ mainPageRef }) => {
+    const buttonPomodoroRef = useRef<HTMLButtonElement>(null)
+    const buttonShortBreakRef = useRef<HTMLButtonElement>(null)
+    const longBreakButtonRef = useRef<HTMLButtonElement>(null)
     const goToPomodoroSection = (): void => {
         if (mainPageRef.current != null) {
-          mainPageRef.current.style.background="rgb(186, 73, 73)"
+            mainPageRef.current.style.background = "rgb(186, 73, 73)"
+        }
+        if (buttonPomodoroRef.current != null) {
+            buttonPomodoroRef.current.style.background = "rgba(255, 255, 255, 0.1)"
+        }
+        if (buttonShortBreakRef.current != null) {
+            buttonShortBreakRef.current.style.background = "none"
+        }
+        if (longBreakButtonRef.current != null) {
+            longBreakButtonRef.current.style.background = "none"
         }
     }
     const goToShortBreakSection = (): void => {
         if (mainPageRef.current != null) {
-            mainPageRef.current.style.background="rgb(56, 133, 138)"
+            mainPageRef.current.style.background = "rgb(56, 133, 138)"
+        }
+        if (buttonPomodoroRef.current != null) {
+            buttonPomodoroRef.current.style.background = "none"
+        }
+        if (buttonShortBreakRef.current != null) {
+            buttonShortBreakRef.current.style.background = "rgba(255, 255, 255, 0.1)"
+        }
+        if (longBreakButtonRef.current != null) {
+            longBreakButtonRef.current.style.background = "none"
         }
     }
     const goToLongBreakSection = (): void => {
         if (mainPageRef.current != null) {
-            mainPageRef.current.style.background="rgb(57, 112, 151)"
+            mainPageRef.current.style.background = "rgb(57, 112, 151)"
+        }
+        if (buttonPomodoroRef.current != null) {
+            buttonPomodoroRef.current.style.background = "none"
+        }
+        if (buttonShortBreakRef.current != null) {
+            buttonShortBreakRef.current.style.background = "none"
+        }
+        if (longBreakButtonRef.current != null) {
+            longBreakButtonRef.current.style.background = "rgba(255, 255, 255, 0.1)"
         }
     }
     return (
@@ -24,9 +56,9 @@ const Timer: React.FC<Props> = ({ mainPageRef }) => {
         <section className="timer-section">
             <div className="timer-container">
                 <div className="buttons-container">
-                    <button onClick={goToPomodoroSection} className="pomodoro-button">Pomodoro</button>
-                    <button onClick={goToShortBreakSection} className="shortBreak-button">Short Break</button>
-                    <button onClick={goToLongBreakSection} className="longBreak-button">Long Break</button>
+                    <button ref={buttonPomodoroRef} onClick={goToPomodoroSection} className="pomodoro-button">Pomodoro</button>
+                    <button ref={buttonShortBreakRef} onClick={goToShortBreakSection} className="shortBreak-button">Short Break</button>
+                    <button ref={longBreakButtonRef} onClick={goToLongBreakSection} className="longBreak-button">Long Break</button>
                 </div>
                 <div className="howMuchTimeLeft-container">
                     <p className="time-paragraph">25:00</p>
